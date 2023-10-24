26aa8299c294ec6f8bd9f4280eccfe0f887e7d85
Mikołaj Cieśliczka <mikolajce@gmail.com>
2023-10-24 23:05:16 +0200

    Revert "hejo"
    This reverts commit d0929a46d592de6f8242384e7aeb2e80d65e7b4a.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\
+{
+    AdminController,
+    AuthController,
+    OrderController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +19,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//App::getRouter()->setDefaultRoute('Main_page'); #default action
+//App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
+
+Route::get('/', fn() => view('home'));
+
+Route::controller(AuthController::class)->group(function()
+{
+    Route::get('/login' , 'login');
+
+    Route::get('/logout', 'logout');
+
+    Route::get('/register', 'register');
+
+    Route::get('/newuser', 'newRegistration');
+
+    Route::get('/showlogin', 'showLogin');
 });
+
+Route::controller(AdminController::class)->group(function ()
+{
+    Route::get('/admin/rolelist', 'roleList');
+
+    Route::get('/admin/delete', 'delete');
+
+    Route::get('/admin/new', 'new');
+
+    Route::get('/admin/makeAdmin', 'makeAdmin');
+
+    Route::get('/admin/makeEmployee', 'makeEmployee');
+
+    Route::get('/admin/makeClient', 'makeClient');
+})
+    ->middleware(['auth']);
+
+Route::controller(OrderController::class)->group(function ()
+{
+    Route::get('/orders', 'allOrders');
+
+    Route::get('/orders/{id}', 'orderDetails');
+
+    Route::get('/menu/pizzas/{id}/add', 'addPizza');
+
+    Route::get('/cart/pizza/remove/{id}', 'removePizza');
+
+    Route::get('/menu/toppings/{id}/add', 'addTopping');
+
+    Route::get('/cart/topping/remove/{id}', 'removeTopping');
+});
+
+//Utils::addRoute('orderList', 'ModCtrl', ["Mod"]);
+
+//Utils::addRoute('deleteOrderMod', 'ModCtrl', ["Mod"]);
+
+//Utils::addRoute('buyPizza', 'OrderCtrl', ["User"]);
